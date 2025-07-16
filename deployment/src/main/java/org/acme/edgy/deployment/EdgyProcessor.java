243903d82f4cd1447a353745d88b27ff4d6c5d78
Julien Ponge <jponge@redhat.com>
2025-07-16 18:39:42 +0200

    WIP

--- deployment/src/main/java/org/acme/edgy/deployment/EdgyProcessor.java
+++ deployment/src/main/java/org/acme/edgy/deployment/EdgyProcessor.java
@@ -3,7 +3,11 @@ package org.acme.edgy.deployment;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import org.acme.edgy.runtime.DynamicRoutingConfigurationProvider;
 import org.acme.edgy.runtime.RouterConfigurator;
+import org.acme.edgy.runtime.config.EdgyConfig;
+
+import java.util.function.BooleanSupplier;
 
 class EdgyProcessor {
 
@@ -17,5 +21,20 @@ class EdgyProcessor {
     @BuildStep
     AdditionalBeanBuildItem addRouterConfigurator() {
         return new AdditionalBeanBuildItem(RouterConfigurator.class);
+    }
+
+    @BuildStep(onlyIf = IsDynamicallyConfigured.class)
+    AdditionalBeanBuildItem addDynamicRoutingProvider() {
+        return new AdditionalBeanBuildItem(DynamicRoutingConfigurationProvider.class);
+    }
+
+    static class IsDynamicallyConfigured implements BooleanSupplier {
+
+        EdgyConfig config;
+
+        @Override
+        public boolean getAsBoolean() {
+            return config.mode() == EdgyConfig.Mode.CONFIGURATION;
+        }
     }
 }
---
+++ runtime/src/main/java/org/acme/edgy/runtime/DynamicRoutingConfigurationProvider.java
@@ -0,0 +1,26 @@
+package org.acme.edgy.runtime;
+
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import org.acme.edgy.runtime.api.RoutingConfiguration;
+import org.acme.edgy.runtime.config.EdgyRoutes;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class DynamicRoutingConfigurationProvider {
+
+    @Inject
+    EdgyRoutes routes;
+
+    @Inject
+    Logger logger;
+
+    @Produces
+    public RoutingConfiguration getFromConfiguration() {
+        // TODO
+        logger.warn("Dynamic routing configuration is not implemented yet");
+        return new RoutingConfiguration();
+    }
+}
